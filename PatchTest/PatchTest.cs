0c93b1b76d4efda098008a41180ec3f6362c257c
LWM <lilmouse@littlewhitemouse.net>
2019-02-24 19:51:14 -0500

    First commit.

---
+++ PatchTest/PatchTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+using RimWorld;
+using Verse;
+
+namespace LWM.PatchTest {
+    public class TimeMessage : PatchOperation {
+        public static Stopwatch stopwatch;
+
+        protected override bool ApplyWorker(System.Xml.XmlDocument xml) {
+            if (command == Command.Start) {
+                stopwatch = new Stopwatch();
+                if (message == "") {
+                    message = "Stopwatch starting at time " + DateTime.Now;
+                } else {
+                    message += " (" + DateTime.Now + ")";
+                }
+                Log.Warning(message);
+                return true;
+            }
+            if (command == Command.TimeNow) {
+                if (message == "") {
+                    message = "Time is currently " + DateTime.Now;
+                } else {
+                    message += " (" + DateTime.Now + ")";
+                }
+                Log.Warning(message);
+                return true;
+            }
+            if (command == Command.Stop) {
+                stopwatch.Stop();
+                TimeSpan ts = stopwatch.Elapsed;
+                string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                    ts.Hours, ts.Minutes, ts.Seconds,
+                    ts.Milliseconds / 10);
+                if (message == "") {
+                    message = "Stopwatch stopping at time " + DateTime.Now;
+                } else {
+                    message += " (" + DateTime.Now + ")";
+                }
+                Log.Warning(message);
+                Log.Error("Time elapsed: " + elapsedTime);
+                return true;
+            }
+            Log.Error("LWM: Patch Test: unknown command?");
+            return false;
+        }
+        protected string message="";
+        protected Command command;
+    }
+    public enum Command : byte {
+        Start,
+        TimeNow,
+        Stop,
+    }
+}
